a75a447812d696cd676bd588903ea40bdcfca3c6
Bianca <biancalway@gmail.com>
2020-07-29 11:43:47 -0300

    Implemented doubly linked list.

---
+++ DoublyLinkedList/DoublyLinkedLIst.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *previous;
+    struct Node *next;
+};
+
+struct Node *head;
+
+void insertHead(int x){
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->previous = NULL;
+    temp->next = NULL;
+
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+
+    head->previous = temp;
+    temp->next = head;
+    head = temp;
+}
+
+void insertTail(int x){
+
+    struct Node *temp1 = head;
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->previous = NULL;
+    temp->next = NULL;
+
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+
+    while(temp1->next != NULL){
+        temp1 = temp1->next;
+    }
+
+    temp1->next = temp;
+    temp->previous = temp1;
+}
+
+void printList(){
+
+    struct Node *temp = head;
+    printf("List: ");
+
+    while(temp != NULL){
+        printf("[%d] ", temp->data);
+        temp = temp->next;
+    }
+
+}
+
+void delete(int n){
+
+    if(n == 0){
+        head = head->next;
+        head->previous = NULL;
+        return;
+    }
+
+    struct Node *temp = head;
+    int i = 0;
+    while(i < n){
+        temp = temp->next;
+        i++;
+    }
+    if(temp->next != NULL){
+        temp->previous->next = temp->next;
+        temp->next->previous = temp->previous;
+    } else{
+        temp->previous->next = NULL;
+    }
+        
+        free(temp);
+}
+
+int main(){
+
+    head = NULL;
+    insertHead(10);
+    insertHead(15);
+    printList();
+    insertTail(20);
+    printList();
+    delete(2);
+    printList();
+
+}
